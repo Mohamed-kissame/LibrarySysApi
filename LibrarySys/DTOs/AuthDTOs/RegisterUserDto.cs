cda1de8fa78f1bb8ec3fe87fa5e08e45e52b0c4f
Mohamed <mkissame9@gmail.com>
2026-07-12 21:14:47 +0100

    Add User Model and AuthDTOs

--- LibrarySys/DTOs/AuthDTOs/RegisterUserDto.cs
+++ LibrarySys/DTOs/AuthDTOs/RegisterUserDto.cs
@@ -1,6 +1,30 @@
-﻿namespace LibrarySys.DTOs.AuthDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySys.DTOs.AuthDTOs
 {
     public class RegisterUserDto
     {
+
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password {  get; set; } = string.Empty;
+
+        public string Role {  get; set; } = string.Empty;
+
+        public int? MemberID { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+
+
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Models
+﻿namespace Models
 {
     public class User
     {
+        public int UserID { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+
+        public int? MemberID { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
     }
 }
